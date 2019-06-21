218b39e491fb40d446d0efb4fcfe9e78b139a132
Joaquín <jmerloperfumo@gmail.com>
2019-06-21 11:42:04 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDQqaXQB9OospXP1-XhcwF5CMgq2aF2EyY",
-    authDomain: "el-buen-sabor-25140.firebaseapp.com",
-    databaseURL: "https://el-buen-sabor-25140.firebaseio.com",
-    projectId: "el-buen-sabor-25140",
-    storageBucket: "el-buen-sabor-25140.appspot.com",
-    messagingSenderId: "16897970458",
-    appId: "1:16897970458:web:bf6e3e72dc1988fc"
+    apiKey: "<<<<<>>>>",
+    authDomain: "<<<<<>>>>",
+    databaseURL: "<<<<<>>>>",
+    projectId: "<<<<<>>>>",
+    storageBucket: "<<<<<>>>>",
+    messagingSenderId: "<<<<<>>>>",
+    appId: "<<<<<>>>>"
   }
 };
 
